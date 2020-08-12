611c2d49f8cd7b8a81f8d43eb9048eb19b28f2e7
IOKernel <chi@cryptik.io>
2020-08-12 05:42:03 +0400

    Initial commit
    Engine initialization, moves entered via CLI

---
+++ chess-bot.py
@@ -0,0 +1,20 @@
+import chess
+import chess.engine
+import os
+import sys
+
+#I hate windows/linux pathing
+script_dir = os.path.abspath(__file__)
+relative_path = "/Engine/stockfish.exe"
+stockfish_loc = script_dir[:-13] + relative_path
+#Stockfish location
+engine = chess.engine.SimpleEngine.popen_uci(stockfish_loc)
+board = chess.Board()
+
+while not board.is_game_over():
+    result = engine.play(board, chess.engine.Limit(time=0.1))
+    print(result.move)
+    playerMove = input()
+    move = chess.Move.from_uci(playerMove)
+    board.push(move)
+engine.quit()
